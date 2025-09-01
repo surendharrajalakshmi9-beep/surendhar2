44265de5e392acfbcd9e360c0ddc47a479def8c4
surendharrajalakshmi9-beep <surendharrajalakshmi9@gmail.com>
2025-09-01 20:53:12 +0530

    Update vite.config.js

--- my-crm-frontend/vite.config.js
+++ my-crm-frontend/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { Card, CardContent } from "./components/ui/card";
+
 
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),  // your alias stays
-    },
+    
   },
   server: {
     proxy: {
