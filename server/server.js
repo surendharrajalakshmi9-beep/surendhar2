e897682554f3142f35959bc903933971e0cd86d2
surendharrajalakshmi9-beep <surendharrajalakshmi9@gmail.com>
2025-09-01 21:38:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ==========================
 // 🔹 Your existing MongoDB connection
