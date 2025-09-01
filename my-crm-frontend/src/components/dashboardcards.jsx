c429db8a16f290ba40721bce4675a06bce08bb32
surendharrajalakshmi9-beep <surendharrajalakshmi9@gmail.com>
2025-09-01 21:05:31 +0530

    Update dashboardcards.jsx

--- my-crm-frontend/src/components/dashboardcards.jsx
+++ my-crm-frontend/src/components/dashboardcards.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom"; 
 import { useState, useEffect } from "react";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card";
 import {
   CheckCircle,
   Clock,
