0b6affbec47b4aeca4f6cc98904d3aacc1769797
surendharrajalakshmi9-beep <surendharrajalakshmi9@gmail.com>
2025-09-01 21:09:56 +0530

    Update login.jsx

--- my-crm-frontend/src/pages/login.jsx
+++ my-crm-frontend/src/pages/login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "../components/ui/card";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
