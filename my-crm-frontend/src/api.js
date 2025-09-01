fe7cab4d8915329e0432be861d7a9ae394d1344c
surendharrajalakshmi9-beep <surendharrajalakshmi9@gmail.com>
2025-09-01 08:29:03 +0530

    Update api.js

--- my-crm-frontend/src/api.js
+++ my-crm-frontend/src/api.js
@@ -1,8 +1,8 @@
 // src/api.js
 const backendUrl = import.meta.env.VITE_API_URL;
 
-export const getData = async () => {
+/*export const getData = async () => {
   const response = await fetch(`${backendUrl}/api/someEndpoint`);
   if (!response.ok) throw new Error("Network response was not ok");
   return response.json();
-};
+};*/
