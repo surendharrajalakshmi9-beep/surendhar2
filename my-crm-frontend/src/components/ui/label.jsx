df275090690c3356891f4afaa20d95c7521071c9
surendharrajalakshmi9-beep <surendharrajalakshmi9@gmail.com>
2025-09-01 21:19:50 +0530

    Update label.jsx

--- my-crm-frontend/src/components/ui/label.jsx
+++ my-crm-frontend/src/components/ui/label.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "../lib/utils"
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -14,3 +14,4 @@ const Label = React.forwardRef(({ className, ...props }, ref) => (
 Label.displayName = LabelPrimitive.Root.displayName
 
 export { Label }
+
