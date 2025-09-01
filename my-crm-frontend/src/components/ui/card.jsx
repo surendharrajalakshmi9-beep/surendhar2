9a214986616b2e2462560f9db59833d4878d5c50
surendharrajalakshmi9-beep <surendharrajalakshmi9@gmail.com>
2025-09-01 21:24:22 +0530

    Update card.jsx

--- my-crm-frontend/src/components/ui/card.jsx
+++ my-crm-frontend/src/components/ui/card.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "../lib/utils"
+import { cn } from "@/lib/utils"
 
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
@@ -48,4 +48,5 @@ const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+
 
