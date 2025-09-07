ae06698273676e92768b195b4dc8f166bf14b4f9
surendharrajalakshmi9-beep <surendharrajalakshmi9@gmail.com>
2025-09-07 13:56:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -916,6 +916,7 @@ app.post("/api/calls/upload", upload.single("file"), async (req, res) => {
         type: row["Warranty Status"] || "",
         natureOfComplaint: row["Nature Of Complaint"] || "",
       }));
+      console.log("date"+row["Complaint date"]);
     } else if (brand === "Atomberg") {
       calls = sheet.map((row) => ({
         brand,
